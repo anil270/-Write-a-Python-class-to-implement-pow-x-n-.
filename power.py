d219d1ed8e9582df4017e8f4a2729497304b5ba4
Anil Kumar <102414580+anil270@users.noreply.github.com>
2022-09-09 23:26:40 +0530

    Add files via upload

---
+++ power.py
@@ -0,0 +1,10 @@
+#Write a Python class to implement pow(x, n).
+
+
+class python:
+    def power(x,y):
+        z=pow(x,y)
+        return z
+x=int(input("Enter the Number :"))
+y=int(input("Enter the exponent :"))
+print(python.power(x,y))
